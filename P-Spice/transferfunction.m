5a0a4481f81c597c531847b071705be706e25aa1
NicolaiFransen <fransendk@gmail.com>
2017-11-22 09:02:04 +0100

    Fedt

--- P-Spice/transferfunction.m
+++ P-Spice/transferfunction.m
@@ -17,11 +17,15 @@ CFB = 9.021*10^(-9);
 R1 = 18000;
 R2 = 2400;
 
-R8 = 10000;
-C8 = 56*10^(-9);
+R8 = 7517;
+C9 = 40.07*10^(-9);
+C10 = 1.38*10^(-9);
 
 Rpar = 2200;
 Gain = R8/Rpar;
+
+wzero = 1 / (2*pi*R8*C9);
+wpole = 1 / (2*pi*R8*((C9*C10)/(C9+C10)));
 
 G0 = ((Rout*Nps)/(Rcs*Acs))*(1/(((1-D)^2/tL) + (2*M)+1));
 %Gs = ((1+s*Cz*Rz)/(s*Cz))*(R1/R2)*(1/(s*CFB*RFB2+1));
@@ -35,33 +39,26 @@ wrhpz = 15851;
 wp1 = 132.807;
 wp2 = 50000;
 
+Gcomp1 = Gain * (1+s/wpole)/(1+s/wzero);
+Gcomp2 = Gain * (1+s/wzero)/(1+s/wpole);
+
 sys = G0 * (((1+s/wesr)*(1-s/wrhpz))/(1+s/wp1)) * (1/(1+(s/wp2)+(s^2/wp2^2)));
 sys1 = sys * (1 / (1+s/314)) * 4.45;
-%sys2 = sys * Gs2;
-%sys3 = sys * Gs3;
+sys2 = sys * Gcomp1;
+sys3 = sys * Gcomp2;
 
 
 figure(1)
 bode(sys)
 margin(sys)
-
+ 
 figure(2)
 bode(sys1)
 margin(sys1)
-% 
-% figure(3)
-% step(sys1)
-% 
-% figure(4)
-% bode(sys * Gain)
-% margin(sys * Gain)
-% 
-% figure(5)
-% step(sys2)
-% 
-% figure(6)
-% bode(sys3)
-% margin(sys3)
+
+figure(3)
+bode(sys3)
+margin(sys3)
 % 
 % figure(7)
 % step(sys3)
